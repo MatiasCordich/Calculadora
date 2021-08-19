82f99197c75b6c474e0cbf7d57e040746d7cc4d6
MatiasCordich <m.scordich@gmail.com>
2021-08-19 01:02:02 -0300

    Calculadora

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// Traigo los valores del display
+
+const displayValorAnterior = document.getElementById('valor-anterior')
+const displayValorActual = document.getElementById('valor-actual')
+
+// Traigo mis botones
+
+const botonesNumeros = document.querySelectorAll('.numero')
+const botonesOperadores = document.querySelectorAll('.operador')
+
+const display = new Display(displayValorAnterior,  displayValorActual)
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () =>  display.agregarNumero(boton.innerHTML));
+})
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+})
+
+
+
